ed4f327411dede338dfebd3b6450f8dfd9794cfd
HaydenWalton12 <70973137+HaydenWalton12@users.noreply.github.com>
2022-09-13 20:45:41 +0100

    Need to make it work

--- Collision.cpp
+++ Collision.cpp
@@ -134,7 +134,7 @@ bool Collision::Intersect(Body* A, Body* B, float dt, Contact& contact)
 		Vec3 vel_a = A->_LinearVelocity;
 		Vec3 vel_b = B->_LinearVelocity;
 
-		if (SphereSphereStatic(a_sphere, b_sphere, pos_a, pos_b, vel_a, vel_b, dt, contact.ptOnA_WorldSpace, contact.ptOnB_WorldSpace))
+		if (SphereSphereDynamic(a_sphere, b_sphere, pos_a, pos_b, vel_a, vel_b, dt, contact.ptOnA_WorldSpace, contact.ptOnB_WorldSpace , contact._TimeOfImpact))
 		{
 
 			A->Update(contact._TimeOfImpact);
